428bb7396b1977d4ea8cf459a12bc72b6404e3ba
Joao Felipe <joaofelipe1294@gmail.com>
2020-09-21 09:07:00 -0300

    Added dinamically generated table name.

--- lib/csv_to_sqlite.rb
+++ lib/csv_to_sqlite.rb
@@ -12,8 +12,9 @@ module CsvToSqlite
     def convert file_path
       connection = CsvToSqlite::Database.new().connect
       csv_table  = CsvToSqlite::CsvReader.load_file file_path
-      CsvToSqlite::SQL::CreateTable.new(name: 'persons', csv_table: csv_table, connection: connection).run
-      CsvToSqlite::SQL::Insert.new(name: 'persons', csv_table: csv_table, connection: connection).run
+      table_name = file_path.split("/").last.split(".csv").first
+      CsvToSqlite::SQL::CreateTable.new(name: table_name, csv_table: csv_table, connection: connection).run
+      CsvToSqlite::SQL::Insert.new(name: table_name, csv_table: csv_table, connection: connection).run
     end
 
   end
